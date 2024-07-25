1d44f11ff4de9f1c7ee5b666e7ffba1ce27b9c13
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2024-07-25 22:48:25 +0100

    Add missing license header

--- src/main/java/com/enderio/regilite/AbstractRegilite.java
+++ src/main/java/com/enderio/regilite/AbstractRegilite.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Team Ender IO and contributors
+ * SPDX-License-Identifier: LGPL-3.0-only
+ */
+
 package com.enderio.regilite;
 
 import com.enderio.regilite.utils.BundledDataProvider;
