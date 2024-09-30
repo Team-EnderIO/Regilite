edac4d03ad44a115ab4978193fecd9bcff83afcd
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2024-09-30 17:19:19 +0100

    build: Fix missing license headers

--- src/main/java/com/enderio/regilite/entities/EntityTypeBuilder.java
+++ src/main/java/com/enderio/regilite/entities/EntityTypeBuilder.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Team Ender IO and contributors
+ * SPDX-License-Identifier: LGPL-3.0-only
+ */
+
 package com.enderio.regilite.entities;
 
 import com.enderio.regilite.RegiliteBuilder;
